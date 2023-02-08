d52e10711ef38559cbb23d1d7fdcc14926bdab06
Long Wan <longwan@microsoft.com>
2023-02-08 09:54:22 -0800

    Longw/kube events timestamp (#898)
    * change collection time
    
    * update and add empty check
    
    * update and add empty check
    
    * update condition check

--- source/plugins/ruby/in_kube_events.rb
+++ source/plugins/ruby/in_kube_events.rb
@@ -152,7 +152,6 @@ module Fluent::Plugin
         events["items"].each do |items|
           record = {}
           #<BUGBUG> - Not sure if ingestion has the below mapping for this custom type. Fix it as part of fixed type conversion
-          record["CollectionTime"] = batchTime #This is the time that is mapped to become TimeGenerated
           eventId = items["metadata"]["uid"] + "/" + items["count"].to_s
           newEventQueryState.push(eventId)
           if !eventQueryState.empty? && eventQueryState.include?(eventId)
@@ -178,7 +177,8 @@ module Fluent::Plugin
           record["Reason"] = items["reason"]
           record["Message"] = items["message"]
           record["KubeEventType"] = items["type"]
-          record["TimeGenerated"] = items["metadata"]["creationTimestamp"]
+          #use metadata time instead of batchTime when metadata time not empty
+          record["CollectionTime"] = items["metadata"].key?("creationTimestamp") ? items["metadata"]["creationTimestamp"] : batchTime
           record["SourceComponent"] = ""
           if items.key?("source") && items["source"].key?("component")
             record["SourceComponent"] = items["source"]["component"]
