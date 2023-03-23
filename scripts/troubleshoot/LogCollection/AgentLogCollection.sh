d7b012b60fdf755aa4ef67ce7f0090b09004d912
Brandon DeGolier <63262146+bdegs@users.noreply.github.com>
2023-03-23 12:29:02 -0400

    Update AgentLogCollection.sh (#935)
    -Resolved issue where script wasn't recognizing daemonset and replicaset pods, thus skipping log collection.
    -Added some additional logging lines to provide visibility of where logs are copied from.
    -Added section to other_logCollection to collect both container-azm-ms-aks-k8scluster as well as ama-logs-rs-config configmap.

--- scripts/troubleshoot/LogCollection/AgentLogCollection.sh
+++ scripts/troubleshoot/LogCollection/AgentLogCollection.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
-#
-# Copyright (c) Microsoft Corporation.
-#
-# This script will collect all logs from the replicaset agent pod and a random daemonset pod, also collect onboard logs with processes
-#
-# Author Nina Li
+#Copyright (c) Microsoft Corporation.
+# Description: This script will collect all logs from the Replicaset Agent Pod and Daemonset Agent Pod.
+# Also collects onboarding logs with processes.
+# Original Author: Nina Li
+# Modified By: Brandon DeGolier
 
 Red='\033[0;31m'
 Cyan='\033[0;36m'
-NC='\033[0m' # No Color
+NC='\033[0m'
 
 init()
 {
     echo -e "Preparing for log collection..." | tee -a Tool.log
 
     if ! cmd="$(type -p kubectl)" || [[ -z $cmd ]]; then
-        echo -e "${Red}Command kubectl not found, please install it firstly, exit...${NC}"
+        echo -e "${Red}Command kubectl not found, please install to continue, exiting...${NC}"
         cd ..
         rm -rf $output_path
         exit
     fi
 
     if ! cmd="$(type -p tar)" || [[ -z $cmd ]]; then
-        echo -e "${Red}Command tar not found, please install it firstly, exit...${NC}"
+        echo -e "${Red}Command tar not found, please install to continue, exiting...${NC}"
         cd ..
         rm -rf $output_path
         exit
@@ -30,7 +29,7 @@ init()
 
     cmd=`kubectl get nodes 2>&1`
     if [[ $cmd == *"refused"* ]];then
-        echo -e "${Red}Fail to connect your AKS, please fisrlty connect to cluster by command: az aks get-credentials --resource-group myResourceGroup --name myAKSCluster${NC}"
+        echo -e "${Red}Failed to connect your AKS, please connect to your cluster by command: az aks get-credentials --resource-group myResourceGroup --name myAKSCluster, exiting...${NC}"
         cd ..
         rm -rf $output_path
         exit
@@ -41,19 +40,19 @@ init()
     do
         if [ `echo $node | tr -s '[:upper:]' '[:lower:]'` != "ready" ]; then
             kubectl get nodes
-            echo -e "${Red} One or more AKS node is not ready, please start this node firstly for log collection, exit...${NC}"
+            echo -e "${Red}One or more AKS node is not ready, please start this node for log collection, exiting...${NC}"
             cd ..
             rm -rf $output_path
             exit
         fi
     done
-    echo -e "Prerequistes check is done, all good" | tee -a Tool.log
+    echo -e "Prerequisites check complete!" | tee -a Tool.log
 
-    echo -e "Saving cluster information" | tee -a Tool.log
+    echo -e "Saving cluster information..." | tee -a Tool.log
     
     cmd=`kubectl cluster-info 2>&1`
     if [[ $cmd == *"refused"* ]];then
-        echo -e "${Red}Fail to get cluster info, please check your AKS status fistly, exit...${NC}"
+        echo -e "${Red}Failed to get cluster info, please check your AKS status, exiting...${NC}"
         cd ..
         rm -rf $output_path
         exit
@@ -76,6 +75,10 @@ ds_logCollection()
     if [[ $cmd == *"cannot access"* ]];then
         echo -e "${Red}/var/opt/microsoft not exist on ${ds_pod}${NC}" | tee -a Tool.log
     else
+        echo -e "Collecting the following logs from ${ds_pod}:"
+        echo -e "/var/opt/microsoft/docker-cimprov/log | Containers "ama-logs, ama-logs-prometheus""
+        echo -e "/var/opt/microsoft/linuxmonagent/log | Containers "ama-logs, ama-logs-prometheus""
+        echo -e "/etc/mdsd.d/config-cache/configchunks/ | Data Collection Rule Config"
         kubectl cp ${ds_pod}:/var/opt/microsoft/docker-cimprov/log ama-logs-daemonset --namespace=kube-system --container ama-logs > /dev/null
         kubectl cp ${ds_pod}:/var/opt/microsoft/docker-cimprov/log ama-logs-prom-daemonset --namespace=kube-system --container ama-logs-prometheus > /dev/null
         kubectl cp ${ds_pod}:/var/opt/microsoft/linuxmonagent/log ama-logs-daemonset-mdsd --namespace=kube-system --container ama-logs > /dev/null
@@ -89,6 +92,8 @@ ds_logCollection()
     if [[ $cmd == *"cannot access"* ]];then
         echo -e "${Red}/etc/fluent not exist on ${ds_pod}${NC}" | tee -a Tool.log
     else
+        echo -e "Collecting the following logs from ${ds_pod}:"
+        echo -e "/etc/fluent/container.conf | Containers "ama-logs, ama-logs-prometheus""
         kubectl cp ${ds_pod}:/etc/fluent/container.conf ama-logs-daemonset/container_${ds_pod}.conf --namespace=kube-system --container ama-logs > /dev/null
         kubectl cp ${ds_pod}:/etc/fluent/container.conf ama-logs-prom-daemonset/container_${ds_pod}_prom.conf --namespace=kube-system --container ama-logs-prometheus > /dev/null
     fi
@@ -97,6 +102,9 @@ ds_logCollection()
     if [[ $cmd == *"cannot access"* ]];then
         echo -e "${Red}/etc/opt/microsoft/docker-cimprov not exist on ${ds_pod}${NC}" | tee -a Tool.log
     else
+        echo -e "Collecting the following logs from ${ds_pod}:"
+        echo -e "/etc/opt/microsoft/docker-cimprov/fluent-bit.conf | Containers "ama-logs, ama-logs-prometheus""
+        echo -e "/etc/opt/microsoft/docker-cimprov/telegraf.conf | Containers "ama-logs, ama-logs-prometheus""
         kubectl cp ${ds_pod}:/etc/opt/microsoft/docker-cimprov/fluent-bit.conf ama-logs-daemonset/fluent-bit.conf --namespace=kube-system --container ama-logs > /dev/null
         kubectl cp ${ds_pod}:/etc/opt/microsoft/docker-cimprov/telegraf.conf ama-logs-daemonset/telegraf.conf --namespace=kube-system --container ama-logs > /dev/null
         kubectl cp ${ds_pod}:/etc/opt/microsoft/docker-cimprov/telegraf.conf ama-logs-prom-daemonset/telegraf.conf --namespace=kube-system --container ama-logs-prometheus > /dev/null
@@ -107,7 +115,7 @@ ds_logCollection()
 
 win_logCollection()
 {
-    echo -e "Collecting logs from ${ds_win_pod}, windows pod will take several minutes for log collection, please dont exit forcely..." | tee -a Tool.log
+    echo -e "Collecting logs from ${ds_win_pod}, Windows pod will take several minutes for log collection, please don't exit..." | tee -a Tool.log
     kubectl describe pod ${ds_win_pod} --namespace=kube-system > describe_${ds_win_pod}.txt
     kubectl logs ${ds_win_pod} --container ama-logs-windows --namespace=kube-system > logs_${ds_win_pod}.txt
     kubectl exec ${ds_win_pod} -n kube-system --request-timeout=10m -- powershell Get-Process > process_${ds_win_pod}.txt
@@ -116,6 +124,9 @@ win_logCollection()
     if [[ $cmd == *"cannot access"* ]];then
         echo -e "${Red}/etc/ not exist on ${ds_pod}${NC}" | tee -a Tool.log
     else
+        echo -e "Collecting the following logs from ${ds_win_pod}:"
+        echo -e "/etc/fluent-bit"
+        echo -e "/etc/telegraf/telegraf.conf"
         kubectl cp ${ds_win_pod}:/etc/fluent-bit ama-logs-windows-daemonset-fbit --namespace=kube-system > /dev/null
         kubectl cp ${ds_win_pod}:/etc/telegraf/telegraf.conf ama-logs-windows-daemonset-fbit/telegraf.conf --namespace=kube-system > /dev/null
 
@@ -146,6 +157,9 @@ rs_logCollection()
     if [[ $cmd == *"cannot access"* ]];then
         echo -e "${Red}/var/opt/microsoft not exist on ${rs_pod}${NC}" | tee -a Tool.log
     else
+        echo -e "Collecting the following logs from ${rs_pod}:"
+        echo -e "/var/opt/microsoft/docker-cimprov/log"
+        echo -e "/var/opt/microsoft/linuxmonagent/log"
         kubectl cp ${rs_pod}:/var/opt/microsoft/docker-cimprov/log ama-logs-replicaset --namespace=kube-system > /dev/null
         kubectl cp ${rs_pod}:/var/opt/microsoft/linuxmonagent/log ama-logs-replicaset-mdsd --namespace=kube-system > /dev/null
     fi
@@ -154,6 +168,8 @@ rs_logCollection()
     if [[ $cmd == *"cannot access"* ]];then
         echo -e "${Red}/etc/fluent not exist on ${rs_pod}${NC}" | tee -a Tool.log
     else
+        echo -e "Collecting the following logs from ${rs_pod}:"
+        echo -e "/etc/fluent/kube.conf"
         kubectl cp ${rs_pod}:/etc/fluent/kube.conf ama-logs-replicaset/kube_${rs_pod}.conf --namespace=kube-system --container ama-logs > /dev/null
     fi
 
@@ -161,6 +177,9 @@ rs_logCollection()
     if [[ $cmd == *"cannot access"* ]];then
         echo -e "${Red}/etc/opt/microsoft/docker-cimprov not exist on ${rs_pod}${NC}" | tee -a Tool.log
     else
+        echo -e "Collecting the following logs from ${rs_pod}:"
+        echo -e "/etc/opt/microsoft/docker-cimprov/fluent-bit-rs.conf"
+        echo -e "/etc/opt/microsoft/docker-cimprov/telegraf-rs.conf"
         kubectl cp ${rs_pod}:/etc/opt/microsoft/docker-cimprov/fluent-bit-rs.conf ama-logs-replicaset/fluent-bit.conf --namespace=kube-system --container ama-logs > /dev/null
         kubectl cp ${rs_pod}:/etc/opt/microsoft/docker-cimprov/telegraf-rs.conf ama-logs-replicaset/telegraf-rs.conf --namespace=kube-system --container ama-logs > /dev/null
     fi
@@ -169,19 +188,29 @@ rs_logCollection()
 
 other_logCollection()
 {
-    echo -e "Collecting onboard logs..."
+    echo -e "Collecting onboarding logs..."
     export deploy=$(kubectl get deployment --namespace=kube-system | grep -E ama-logs | head -n 1 | awk '{print $1}')
     if [ -z "$deploy" ];then
-        echo -e "${Red}there is not ama-logs deployment, skipping log collection of deployment${NC}" | tee -a Tool.log
+        echo -e "${Red}There is no ama-logs deployment present, skipping log collection of deployment${NC}" | tee -a Tool.log
     else
+        echo -e "Collecting deployment info..."
         kubectl get deployment $deploy --namespace=kube-system -o yaml > deployment_${deploy}.txt
     fi
 
-    export config=$(kubectl get configmaps --namespace=kube-system | grep -E container-azm-ms-agentconfig | head -n 1 | awk '{print $1}')
+    export config=$(kubectl get configmaps --namespace=kube-system | grep -E container-azm-ms-aks-k8scluster | head -n 1 | awk '{print $1}')
     if [ -z "$config" ];then
-        echo -e "${Red}configMap named container-azm-ms-agentconfig is not found, if you created configMap for ama-logs, please manually save your custom configMap of ama-logs by command: kubectl get configmaps <configMap name> --namespace=kube-system -o yaml > configMap.yaml${NC}" | tee -a Tool.log
+        echo -e "${Red}configMap named container-azm-ms-aks-k8scluster is not found, if you created configMap for ama-logs, please manually save your custom configMap of ama-logs by command: kubectl get configmaps <configMap name> --namespace=kube-system -o yaml > configMap.yaml${NC}" | tee -a Tool.log
     else
+        echo -e "Collecting container-azm-ms-aks-k8scluster configmap..."
         kubectl get configmaps $config --namespace=kube-system -o yaml > ${config}.yaml
+    fi
+
+    export config2=$(kubectl get configmaps --namespace=kube-system | grep -E ama-logs-rs-config | head -n 1 | awk '{print $1}')
+    if [ -z "$config2" ];then
+        echo -e "${Red}configMap named ama-logs-rs-config is not found, if you created configMap for ama-logs, please manually save your custom configMap of ama-logs by command: kubectl get configmaps <configMap name> --namespace=kube-system -o yaml > configMap.yaml${NC}" | tee -a Tool.log
+    else
+        echo -e "Collecting ama-logs-rs-config configmap..."
+        kubectl get configmaps $config2 --namespace=kube-system -o yaml > ${config2}.yaml
     fi
 
     kubectl get nodes > node.txt
@@ -191,10 +220,10 @@ other_logCollection()
         To check current version and upgrade: https://learn.microsoft.com/en-us/azure/aks/node-image-upgrade"
     kubectl get nodes -o json > node-detailed.json
 
-    echo -e "Complete onboard log collection!" | tee -a Tool.log
+    echo -e "Complete onboarding log collection!" | tee -a Tool.log
 }
 
-#main
+#Main
 output_path="AKSInsights-logs.$(date +%s).`hostname`"
 mkdir -p $output_path
 cd $output_path
@@ -202,22 +231,22 @@ cd $output_path
 init
 
 export ds_pod=$(kubectl get pods -n kube-system -o custom-columns=NAME:.metadata.name | grep -E ama-logs-[a-z0-9]{5} | head -n 1)
-if [ -z "$ds_pod" ];then
-	echo -e "${Red}daemonset pod do not exist, skipping log collection for daemonset pod${NC}" | tee -a Tool.log
+if [[ -z "$ds_pod" ]];then
+	echo -e "${Red}Daemonset pod does not exist, skipping log collection for daemonset pod${NC}" | tee -a Tool.log
 else
     ds_logCollection
 fi
 
 export ds_win_pod=$(kubectl get pods -n kube-system -o custom-columns=NAME:.metadata.name | grep -E ama-logs-windows-[a-z0-9]{5} | head -n 1)
-if [ -z "$ds_win_pod" ];then
-	echo -e "${Cyan} windows agent pod do not exist, skipping log collection for windows agent pod ${NC}" | tee -a Tool.log
+if [[ -z "$ds_win_pod" ]];then
+	echo -e "${Cyan}Windows agent pod does not exist, skipping log collection for windows agent pod ${NC}" | tee -a Tool.log
 else
     win_logCollection
 fi
 
 export rs_pod=$(kubectl get pods -n kube-system -o custom-columns=NAME:.metadata.name | grep -E ama-logs-rs-[a-z0-9]{5} | head -n 1)
-if [ -z "$rs_pod" ];then
-	echo -e "${Red}replicaset pod do not exist, skipping log collection for replicaset pod ${NC}" | tee -a Tool.log
+if [[ -z "$rs_pod" ]];then
+	echo -e "${Red}replicaset pod does not exist, skipping log collection for replicaset pod ${NC}" | tee -a Tool.log
 else
     rs_logCollection
 fi
