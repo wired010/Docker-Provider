ae617ccf85444660df4641f5a9553d72c4f29abb
Amol Agrawal <pfrcks@gmail.com>
2023-04-10 12:06:53 -0700

    enable telegraf in rs for npm metrics (#947)
    * enable telegraf on rs if npm metrics enabled
    
    * add vuln
    
    * address PR comment

--- kubernetes/linux/main.sh
+++ kubernetes/linux/main.sh
@@ -608,6 +608,12 @@ else
       setGlobalEnvVar TELEMETRY_CUSTOM_PROM_MONITOR_PODS false
 fi
 
+# If Azure NPM metrics is enabled turn telegraf on in RS
+if [[ ( "${TELEMETRY_NPM_INTEGRATION_METRICS_BASIC}" -eq 1 ) ||
+      ( "${TELEMETRY_NPM_INTEGRATION_METRICS_ADVANCED}" -eq 1 ) ]]; then
+      setGlobalEnvVar TELEMETRY_RS_TELEGRAF_DISABLED false
+fi
+
 #Parse sidecar agent settings for custom configuration
 if [ ! -e "/etc/config/kube.conf" ]; then
       if [ "${CONTAINER_TYPE}" == "PrometheusSidecar" ]; then
