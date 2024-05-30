038e7f3a9544d20c3d29a15c6a0ffa617c515320
Ganga Mahesh Siddem <gangams@microsoft.com>
2024-05-30 15:59:41 -0700

    update fluent-bit for security patches (#1269)

--- kubernetes/linux/setup.sh
+++ kubernetes/linux/setup.sh
@@ -78,7 +78,7 @@ docker_cimprov_version=$(sudo tdnf list installed | grep docker-cimprov | awk '{
 echo "DOCKER_CIMPROV_VERSION=$docker_cimprov_version" >> packages_version.txt
 
 #install fluent-bit
-sudo tdnf install fluent-bit-2.2.2 -y
+sudo tdnf install fluent-bit-2.2.3 -y
 echo "$(fluent-bit --version)" >> packages_version.txt
 
 # install fluentd using the mariner package
